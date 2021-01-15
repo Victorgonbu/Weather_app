06679ce4b7ad7f2af6977bae5c6834a012fcbff1
victorG <peter200970@hotmail.com>
2021-01-15 15:38:49 -0500

    add Webpack setup and creates source files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/weather_components/weather_script.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+}
