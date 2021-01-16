0d4343e8fb00c56056dd50bea011acb86913522a
victorG <peter200970@hotmail.com>
2021-01-16 17:18:08 -0500

    Refactor all weather code into small functions to retrieve information

--- src/weather_components/weather.js
+++ src/weather_components/weather.js
@@ -18,22 +18,52 @@ const weather = (request) => {
     const cityName = (data) => {
         return data.name + ',';
     };
+    
+    const countryName = (data) => {
+        return data.sys.country;
+    };
+    
+    const temperature = (data) => {
+        return `<span>${Math.trunc(data.main.temp)}</span>`;
+    };
+
+    const weatherDescription = (data) => {
+        return data.weather[0].description;
+    };
+
+    const feelsLike = (data) => {
+        return Math.trunc(data.main.feels_like);
+    };
+
+    const windForce = (data) => {
+        return data.wind.speed + ' Km/h';
+    };
+
+    const humidity = (data) => {
+        return data.main.humidity + '%';
+    };
+
+    const getIconURL = (data) => {
+        let icon = data.weather[0].icon;
+        return `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    };
+
 
     const retrieveInfo = () => {
         request.then(data => {
             removeErrorMessage()
             cityTag.textContent = cityName(data);
-            countryTag.textContent = data.sys.country;
-            temperatureTag.innerHTML = `<span>${Math.trunc(data.main.temp)}</span>`;
-            weatherDescription.textContent = data.weather[0].description;
-            feelsLikeTag.textContent = Math.trunc(data.main.feels_like);
-            windTag.textContent = data.wind.speed + ' Km/h';
-            humidity.textContent = data.main.humidity + '%';
-            let icon = data.weather[0].icon;
-            iconContainer.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            countryTag.textContent = countryName(data);
+            temperatureTag.innerHTML = temperature(data);
+            weatherDescription.textContent = weatherDescription(data);
+            feelsLikeTag.textContent = feelsLike(data);
+            windTag.textContent = windForce(data);
+            humidity.textContent = humidity(data);
+            
+            iconContainer.src = getIconURL(data);
         }).catch(error => {
             searchError.classList.add('search-error-error');
-        });
+        }); 
     };
 
 
