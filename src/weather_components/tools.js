5c0191da0847e80756468b40a09c02b1a813f630
victorG <peter200970@hotmail.com>
2021-01-16 02:33:11 -0500

    Add display for data in html markup

--- src/weather_components/tools.js
+++ src/weather_components/tools.js
@@ -1,18 +1,11 @@
 const tools = (() => {
  
-const capitalize = (string) => {
-    string = string.split('');
-    string[0].toUpperCase();
-    string = string.join('');
-    return string
-};
-
 const enterKeyCode = () => {
     return 13;
 };
 
 const getSearchURL = (search, config) => {
-        capitalize(search);
+       
         let searchValues = search.split(' ').join(',');
 
         return `https://api.openweathermap.org/data/2.5/weather?q=${searchValues}&appid=${config.API_KEY}&units=metric`;
@@ -33,7 +26,6 @@ const tryRequest = async (url) => {
 };
 
 return {
-    capitalize,
     enterKeyCode,
     getSearchURL,
     tryRequest
--- src/weather_components/weather_script.js
+++ src/weather_components/weather_script.js
@@ -11,8 +11,11 @@ const cityTag = document.querySelector('.city');
 const countryTag = document.querySelector('.country');
 const temperatureTag = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
-
+const feelsLikeTag = document.querySelector('.feel-like');
+const windTag = document.querySelector('.wind');
 const searchInput = document.querySelector('.search-input');
+const humidity = document.querySelector('.humidity');
+const iconContainer = document.querySelector('.icon-container');
 
 searchInput.addEventListener('keypress', (e) => {
     
@@ -31,8 +34,13 @@ searchInput.addEventListener('keypress', (e) => {
                 console.log(data);
                 cityTag.textContent = data.name + ', ';
                 countryTag.textContent = data.sys.country;
-                temperatureTag.innerHTML = `<span>${data.main.temp}</span> <i class='fas fa-dot-circle'></i>`;
+                temperatureTag.innerHTML = `<span>${Math.trunc(data.main.temp)}</span>`;
                 weatherDescription.textContent = data.weather[0].description;
+                feelsLikeTag.textContent = Math.trunc(data.main.feels_like);
+                windTag.textContent = data.wind.speed + ' Km/h';
+                humidity.textContent = data.main.humidity + '%';
+                let icon = data.weather[0].icon;
+                iconContainer.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
             });
         }
     }
